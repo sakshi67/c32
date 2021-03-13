c3ebcfe3fb723586ca19b339e2c4bc254a606048
sakshi67 <skpgh2007@gmail.com>
2021-03-13 19:14:33 +0530

    adding time & date

--- sketch.js
+++ sketch.js
@@ -3,15 +3,17 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var engine, world;
+var engine, world,;
 var box1, pig1,pig3;
-var backgroundImg,platform;
+var backgroundimg,platform;
 var bird, slingshot;
+var score=0;
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+   
+    getTime(); 
 }
 
 function setup(){
@@ -45,18 +47,26 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundimg){
+    background(backgroundimg);
+
+textSize(35);
+fill("white");
+text("Score:" + score , 900,50)
+
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -66,8 +76,9 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
-}
+    slingshot.display(); 
+     
+}}
 
 function mouseDragged(){
     if (gameState!=="launched"){
@@ -85,4 +96,24 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getTime(){
+  var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Tokyo")  ;
+var responseJson = await response.json();
+console.log(responseJson.datetime);
+
+var datetime = responseJson.datetime;
+var hour = datetime.slice(11,13);
+
+if(hour>=06 && hour<=19){
+    bg="sprites/bg.png";
+}
+else
+{
+    bg="sprites/bg2.jpg";
+}
+
+backgroundimg= loadImage(bg);
+console.log(backgroundimg);
 }
